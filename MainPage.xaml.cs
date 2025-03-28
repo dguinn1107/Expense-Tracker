0114472f46d2fdae40eac825982ed0aa3baa7bda
Daigeona Guinn <dguinn1107@gmail.com>
2025-03-27 20:38:51 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpenseTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
